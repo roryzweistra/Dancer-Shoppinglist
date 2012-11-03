3b59a44ee0ae24f63e7c9f45faa9da55a535b4da
roryzweistra <rory@oqapi.nl>
2012-11-03 23:48:41 +0100

    Changed method of loading Routes modules.

--- Inventory/lib/Inventory.pm
+++ Inventory/lib/Inventory.pm
@@ -1,13 +1,26 @@
 package Inventory;
 use Dancer ':syntax';
 
-use Inventory::Routes::Items;
-use Inventory::Routes::Item;
-
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'index';
+get '/' 		=> sub {
+	template 'index';
 };
+
+get '/inventories'	=> sub {
+	template 'inventories';
+};
+
+get '/profile'		=> sub {
+	template 'profile';
+};
+
+get '/login'		=> sub {
+	template 'login';
+};
+
+load_app 'Inventory::Routes::Items';
+load_app 'Inventory::Routes::Item';
+load_app 'Inventory::Routes::Inventories';
 
 true;
