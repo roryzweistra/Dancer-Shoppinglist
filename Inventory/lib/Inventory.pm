b231c4ff1e9e852f490478d07862f72fe6ed6320
roryzweistra <rory@oqapi.nl>
2012-11-04 14:41:52 +0100

    Created account registration templates and functions.

--- Inventory/lib/Inventory.pm
+++ Inventory/lib/Inventory.pm
@@ -19,6 +19,14 @@ get '/login'		=> sub {
 	template 'login';
 };
 
+get '/register'		=> sub {
+	template 'register', {}, { layout => 0 };
+};
+
+post '/account/create'	=> sub {
+	return 1;
+};
+
 load_app 'Inventory::Routes::Items';
 load_app 'Inventory::Routes::Item';
 load_app 'Inventory::Routes::Inventories';
--- Inventory/public/javascripts/inventory.js
+++ Inventory/public/javascripts/inventory.js
@@ -1,6 +1,8 @@
 $( document ).ready( function() {
 	init_pjax();
 	wire_create_inventory();
+	wire_register_form_button();
+	wire_register_account_button();
 });
 
 var init_pjax = function() {
@@ -20,6 +22,8 @@ var init_pjax = function() {
                 //$( '#loader'    ).hide();
                 $( '#main'      ).fadeIn('slow');
 		wire_create_inventory();
+		wire_register_form_button();
+		wire_register_account_button();
         });
 };
 
@@ -30,6 +34,44 @@ var wire_create_inventory = function() {
 			url		: '/inventories',
 			container	: '#main',
 			fragment	: '#pjax'
+		});
+	});
+};
+
+var wire_register_form_button = function() {
+	$( '#register_button' ).click( function( e ) {
+		e.preventDefault();
+
+		
+		$.ajax({
+                	type    : 'GET',
+                	url     : '/register',
+                	success : function( data ) {
+				$( '#login_user'	).addClass( 'disabled'		);
+				$( '#emailField' 	).attr( 'disabled', 'yes' 	);
+				$( '#passwordField'	).attr( 'disabled', 'yes' 	);
+				$( '#login_button'	).attr( 'disabled', 'yes' 	);
+				$( '#register_section' ).html( data );
+				wire_register_account_button();
+                        }
+                });
+	});
+};
+
+var wire_register_account_button = function() {
+	$( '#register_user' ).submit( function( e ) {
+		e.preventDefault();
+
+		$.ajax({
+			type	: 'POST',
+			url	: '/account/create',
+			success	: function( data ) {
+				$( '#login_user'	).removeClass( 'disabled'	);
+				$( '#emailField' 	).removeAttr( 'disabled'	);
+				$( '#passwordField'	).removeAttr( 'disabled'	);
+				$( '#login_button'	).removeAttr( 'disabled'	);
+				$( '#register_section' ).html('<h1>Your account has been created.</h1><div>Thanks for registering an account with us. You shortly will receive an email with confirmation link. Please click on the link and confirm that you wish to register for this website.</div>');
+			}
 		});
 	});
 };
